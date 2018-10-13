c9f010aada32a1eff9fcd1abb81529940a2dbe14
Marius S Kristiansen <mariussk@getacademy.no>
2018-10-14 00:05:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,90 @@
 var lastScrollTop = 0;
             var yPosition = 0;
             var amount = 5;
-            // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
             window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
             var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
             if (st > lastScrollTop){
-                // downscroll code
                 yPosition -= amount;
-                //console.log('Scroll Down');
                 document.getElementsByTagName('body')[0].style.backgroundPositionY = yPosition + "px";
             } else {
-                // upscroll code
                 yPosition += amount;
-                //console.log('Scroll Up');
                 document.getElementsByTagName('body')[0].style.backgroundPositionY = yPosition+"px";
-
             }
             lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
 }, false);
 
+var fireworks = [];
+var particles = [];
 
+class Particle {
+    constructor(posX, posY, gravity=10, decay=0.8) {
+        this.posX = posX;
+        this.posY = posY;
+        this.velX = 0;
+        this.velY = 0;
+        this.accX;
+        this.accY;
+        this.lifespan;
+        this.decay = decay;
+        this.gravity = gravity;
+    }
+
+    update() {
+        this.velX += this.accX;
+        this.velY += this.accY += this.gravity;
+        this.posX += this.velX;
+        this.posY += this.velY;
+        this.accX *= this.decay;
+        this.accY *= this.decay;
+    }
+
+    show() { //Update the position of the element
+
+    }
+}
+
+class Firework {
+    constructor(gravity=10, decay=0.8) {
+        this.posX = width.innerWidth / 2;
+        this.posY = window.innerHeight;
+        this.velX;
+        this.velY;
+        this.accX;
+        this.accY;
+        this.lifespan;
+        this.timer = 100;
+        this.decay = decay;
+        this.gravity = gravity;
+
+    }
+
+    update() {
+        this.velX += this.accX;
+        this.velY += this.accY += this.gravity;
+        this.posX += this.velX;
+        this.posY += this.velY;
+        this.accX *= this.decay;
+        this.accY *= this.decay;
+    }
+
+    show() { //Update the position of the elemnt
+        
+    }
+
+    explode() {
+
+    }
+
+}
+
+function setup() {
+
+}
+
+function loop() {
+
+}
+
+function main() {
+
+}
