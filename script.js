654df651f7eca7e4f96826cf935871373000ad79
Marius S Kristiansen <mariussk@getacademy.no>
2018-10-13 18:20:26 +0200

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var lastScrollTop = 0;
             var yPosition = 0;
-            var amount = 10;
+            var amount = 5;
             // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
             window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
             var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
