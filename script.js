b0a0334d7b1b66e734298f6b2741a6c06819b321
Marius S Kristiansen <mariussk@getacademy.no>
2018-10-17 14:50:28 +0200

    update

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var lastScrollTop = 0;
-            var yPosition = 0;
-            var amount = 5;
-            window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
-            var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-            if (st > lastScrollTop){
-                yPosition -= amount;
-                document.getElementsByTagName('body')[0].style.backgroundPositionY = yPosition + "px";
-            } else {
-                yPosition += amount;
-                document.getElementsByTagName('body')[0].style.backgroundPositionY = yPosition+"px";
-            }
-            lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-}, false);
+var yPosition = 0;
+var amount = 5;
+window.addEventListener("scroll", function(){ 
+    var st = window.pageYOffset || document.documentElement.scrollTop; 
+    if (st > lastScrollTop){
+        yPosition -= amount;
+        document.getElementsByTagName('body')[0].style.backgroundPositionY = yPosition + "px";
+    } else {
+        yPosition += amount;
+        document.getElementsByTagName('body')[0].style.backgroundPositionY = yPosition+"px";
+    }
+    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+    }, false);
 
 var cards = [
     { id: 0, name: 'geit', src: 'img/geit_display.jpg', class: 'display'},
